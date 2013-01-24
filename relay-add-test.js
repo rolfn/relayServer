89102d5c0765b9bc9d12812f2423551f2dcf87ee
wactbprot <thsteinbock@web.de>
2013-01-24 11:25:08 +0100

    Comments 4 add-test

--- relay-add-test.js
+++ relay-add-test.js
@@ -1,34 +1,38 @@
 #!/usr/bin/env node
+/**
+ *
+ * Test der Postprocessing funktionen.
+ *
+ * @author wactbprot (thsteinbock@web.de)
+ */
+
 var add =require("./relay-add.js");
-
-
-
 
 /**
  * Test der extractKeithley  function
  */
 var s = [ '-1.77311995E-05VDC,+1210.560SECS,+09883RDNG#\n',
-	  '-1.80353454E-05VDC,+1210.666SECS,+09884RDNG#\n',
-	  '-1.80762017E-05VDC,+1210.878SECS,+09886RDNG#\n',
-	  '-1.82509721E-05VDC,+1210.985SECS,+09887RDNG#\n',
-	  '-1.82509721E-05VDC,+1210.985SECS,+09887RDNG#\n',
-	  '-1.77675156E-05VDC,+1211.091SECS,+09888RDNG#\n',
-	  '-1.76063622E-05VDC,+1211.408SECS,+09890RDNG#\n',
-	  '-1.76040921E-05VDC,+1211.514SECS,+09891RDNG#\n',
-	  '-1.79604449E-05VDC,+1211.620SECS,+09892RDNG#\n',
-	  '-1.81658579E-05VDC,+1211.726SECS,+09893RDNG#\n',
-	  '-1.81454288E-05VDC,+1211.939SECS,+09895RDNG#\n',
-	  '-1.81454288E-05VDC,+1211.939SECS,+09895RDNG#\n',
-	  '-1.78639802E-05VDC,+1212.046SECS,+09896RDNG#\n',
-	  '-1.76460835E-05VDC,+1212.258SECS,+09897RDNG#\n',
-	  '-1.72091550E-05VDC,+1212.470SECS,+09899RDNG#\n',
-	  '-1.79309372E-05VDC,+1212.576SECS,+09900RDNG#\n',
-	  '-1.80308070E-05VDC,+1212.683SECS,+09901RDNG#\n',
-	  '-1.78140453E-05VDC,+1212.789SECS,+09902RDNG#\n',
-	  '-1.72182336E-05VDC,+1212.895SECS,+09903RDNG#\n',
-	  '-1.81261366E-05VDC,+1213.002SECS,+09904RDNG#\n' ];
+          '-1.80353454E-05VDC,+1210.666SECS,+09884RDNG#\n',
+          '-1.80762017E-05VDC,+1210.878SECS,+09886RDNG#\n',
+          '-1.82509721E-05VDC,+1210.985SECS,+09887RDNG#\n',
+          '-1.82509721E-05VDC,+1210.985SECS,+09887RDNG#\n',
+          '-1.77675156E-05VDC,+1211.091SECS,+09888RDNG#\n',
+          '-1.76063622E-05VDC,+1211.408SECS,+09890RDNG#\n',
+          '-1.76040921E-05VDC,+1211.514SECS,+09891RDNG#\n',
+          '-1.79604449E-05VDC,+1211.620SECS,+09892RDNG#\n',
+          '-1.81658579E-05VDC,+1211.726SECS,+09893RDNG#\n',
+          '-1.81454288E-05VDC,+1211.939SECS,+09895RDNG#\n',
+          '-1.81454288E-05VDC,+1211.939SECS,+09895RDNG#\n',
+          '-1.78639802E-05VDC,+1212.046SECS,+09896RDNG#\n',
+          '-1.76460835E-05VDC,+1212.258SECS,+09897RDNG#\n',
+          '-1.72091550E-05VDC,+1212.470SECS,+09899RDNG#\n',
+          '-1.79309372E-05VDC,+1212.576SECS,+09900RDNG#\n',
+          '-1.80308070E-05VDC,+1212.683SECS,+09901RDNG#\n',
+          '-1.78140453E-05VDC,+1212.789SECS,+09902RDNG#\n',
+          '-1.72182336E-05VDC,+1212.895SECS,+09903RDNG#\n',
+          '-1.81261366E-05VDC,+1213.002SECS,+09904RDNG#\n' ];
 
-console.log("extractKeithley");
+console.log("~~~~~~~~~~~~~extractKeithley~~~~~~~~~~~~~");
 console.log(s.map(add.extractKeithley));
 
 
@@ -36,32 +40,34 @@ console.log(s.map(add.extractKeithley));
  * Test der extractF250  function
  */
 var s = ["A 23.234C\r\n","A 23.23C\r\n", "A23.45C","A 23.23"];
-
+console.log("~~~~~~~~~~~~~add.extractF250~~~~~~~~~~~~~");
 console.log(s.map(add.extractF250));
 
 /**
  * Test der extractAtmion  function
  */
 var s = ['0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r',
-  '0,\t1.9600E-09\r' ];
+         '0,\t1.9600E-09\r',
+         '0,\t1.9600E-09\r',
+         '0,\t1.9600E-09\r',
+         '0,\t1.9600E-09\r',
+         '0,\t1.9600E-09\r',
+         '0,\t1.9600E-09\r',
+         '0,\t1.9600E-09\r',
+         '0,\t1.9600E-09\r',
+         '0,\t1.9600E-09\r',
+         '0,\t1.9600E-09\r',
+         '0,\t1.9600E-09\r',
+         '0,\t1.9600E-09\r',
+         '0,\t1.9600E-09\r',
+         '0,\t1.9600E-09\r',
+         '0,\t1.9600E-09\r',
+         '0,\t1.9600E-09\r',
+         '0,\t1.9600E-09\r',
+         '0,\t1.9600E-09\r',
+         '0,\t1.9600E-09\r' ];
+
+console.log("~~~~~~~~~~~~~extractAtmion~~~~~~~~~~~~~");
 var _vec = s.map(add.extractAtmion);
 console.log(_vec);
 _ret =add.checkNumArr(_vec);
@@ -82,9 +88,12 @@ var arr =  [1.14223873,
             -1.39850969,
             -0.24774159]
 
+console.log("~~~~~~~~~~~~~vlStat~~~~~~~~~~~~~");
 console.log(add.vlStat(arr));
 
-
+/**
+ * Test der checkNumArr function
+ */
 var arr =  [1.14223873,
             '',
             -0.45056385,
@@ -96,17 +105,21 @@ var arr =  [1.14223873,
             -1.39850969,
             -0.24774159]
 
+console.log("~~~~~~~~~~~~~checkNumArr~~~~~~~~~~~~~");
 console.log(arr);
 console.log(add.checkNumArr(arr))
 
 
 /**
- * ergibt:
+ *
+ *  Test der Funktion slope
+ *
+ * JS errechnet:
  * { mv: 0.07972092500000003, <<-- 1
  * N: 10,
  * sd: 1.078706836758906 }    <<-- 2
  *
- *R liefert:
+ * R liefert:
  * >  arr <- c(1.14223873 , 0.01283644 ,-0.45056385 , 1.17646101,
  *             0.30347168 , 0.01957568 , -1.53945684  ,1.77889768,
  *            -1.39850969, -0.24774159)
@@ -116,81 +129,82 @@ console.log(add.checkNumArr(arr))
  * [1] 0.07972093              <<-- 1
  */
 
-/**
- * Test der Funktion slope
- *
- **/
-
 var raw_vec = [ 'MEASURING  290.64E-3',
-		'MEASURIN3',
-		'MEASURING  290.67E-3',
-		'MEASURING  290.64E-3',
-		'MEASURING  290.65E-3',
-		'MEASURING  90.64E-3',
-		'MEASURING  290.63E-3',
-		'MEASURING  290.64E-3',
-		'MEASURING  290.64E-3',
-		'MEASURING  290.65E-3',
-		'MEASURING  290.67E-3',
-		'MEASURING  290.65E-3',
-		'MEASURING  290.70E-3',
-		'MEASURING  290.64E-3',
-		'MEASURING  290.62E-3',
-		'MEASURING  290.68E-3',
-		'MEASURING  290.63E-3',
-		'MEASURING  290.67E-3',
-		'MEASURING  290.69E-3',
-		'MEASURING  290.69E-3' ],
+                'MEASURIN3',
+                'MEASURING  290.67E-3',
+                'MEASURING  290.64E-3',
+                'MEASURING  290.65E-3',
+                'MEASURING  90.64E-3',
+                'MEASURING  290.63E-3',
+                'MEASURING  290.64E-3',
+                'MEASURING  290.64E-3',
+                'MEASURING  290.65E-3',
+                'MEASURING  290.67E-3',
+                'MEASURING  290.65E-3',
+                'MEASURING  290.70E-3',
+                'MEASURING  290.64E-3',
+                'MEASURING  290.62E-3',
+                'MEASURING  290.68E-3',
+                'MEASURING  290.63E-3',
+                'MEASURING  290.67E-3',
+                'MEASURING  290.69E-3',
+                'MEASURING  290.69E-3' ],
 t_start = [ 1348666058346,
-	    1348666059342,
-	    1348666060345,
-	    1348666061059,
-	    1348666062062,
-	    1348666063064,
-	    1348666064065,
-	    1348666065069,
-	    1348666066072,
-	    1348666067075,
-	    1348666068077,
-	    1348666069080,
-	    1348666070082,
-	    1348666071084,
-	    1348666072087,
-	    1348666073095,
-	    1348666074099,
-	    1348666075102,
-	    1348666076103,
-	    1348666077106 ],
+            1348666059342,
+            1348666060345,
+            1348666061059,
+            1348666062062,
+            1348666063064,
+            1348666064065,
+            1348666065069,
+            1348666066072,
+            1348666067075,
+            1348666068077,
+            1348666069080,
+            1348666070082,
+            1348666071084,
+            1348666072087,
+            1348666073095,
+            1348666074099,
+            1348666075102,
+            1348666076103,
+            1348666077106 ],
 t_stop = [ 1348666058399,
-	   1348666059377,
-	   1348666060379,
-	   1348666061091,
-	   1348666062094,
-	   1348666063096,
-	   1348666064098,
-	   1348666065124,
-	   1348666066115,
-	   1348666067107,
-	   1348666068113,
-	   1348666069112,
-	   1348666070135,
-	   1348666071117,
-	   1348666072119,
-	   1348666073127,
-	   1348666074135,
-	   1348666075139,
-	   1348666076145,
-	   1348666077140 ];
+           1348666059377,
+           1348666060379,
+           1348666061091,
+           1348666062094,
+           1348666063096,
+           1348666064098,
+           1348666065124,
+           1348666066115,
+           1348666067107,
+           1348666068113,
+           1348666069112,
+           1348666070135,
+           1348666071117,
+           1348666072119,
+           1348666073127,
+           1348666074135,
+           1348666075139,
+           1348666076145,
+           1348666077140 ];
 
- var vec = raw_vec.map(add.extractValue);
+console.log("~~~~~~~~~~~~~vlSlope~~~~~~~~~~~~~");
+var vec = raw_vec.map(add.extractValue);
+console.log(add.vlSlope(vec,t_start,t_stop));
 
-console.log(add.vlSlope(vec,t_start,t_stop))
 
+/**
+ *
+ *  Test der Funktion se1ValveClosed
+ *
+ */
 
 var v6zu  = '00310EFE0E\r';
-var v6auf = '00112EFE2E\r';       
+var v6auf = '00112EFE2E\r';
 
-console.log("se1ValveClosed");
+console.log("~~~~~~~~~~~~~se1ValveClosed~~~~~~~~~~~~~");
 console.log("zu");
 console.log(add.se1ValveClosed(v6zu, "V6"));
 console.log("auf");
--- relay-add.js
+++ relay-add.js
@@ -1,7 +1,7 @@
 /**
  * Dedizierte Funktionen zur Vereinfachung des
  * Postprocessing einer Task.
- * 
+ *
  * Eine hier definierte Funktion
  * z.B. helloSandbox() kann über:
  *
@@ -10,66 +10,65 @@
  * im Postprocessing ausgeführt werden.
  *
  * @author wactbprot (thsteinbock@web.de)
- * version: 2013-01-18
  */
- 
+
 /**
  * Berechnet, in Abhängigkeit von Ziel- und Istdruck
  * den aktuellen Sollfluß für die FM3 Fülldruckeinstellung.
  *
  *
- * @author wactbprot
+ *
  * @param psoll Number Solldruck in mbar
  * @param psoll Number Istdruck in mbar
  * @param Vopen Number kleines oder großes Volumen
- * @return Object Zielfluß bzw Setpoint für Regler 1&2 
+ * @return Object Zielfluß bzw Setpoint für Regler 1&2
  */
-function fm3CalQsp(psoll, pist, Vopen) { 
-  if (psoll && pist && typeof Vopen == "boolean") {
-    var ret = {},
-    pfill_ok = false,               // Fülldruck ok
-      mq = Vopen ? 0.0133 : 0.0155, // experimentall best. Steigungen
-      ts = 10,                      // Sollzeit
-      bord = 0.8,                   // Start Regelung rel. Abw. von psoll
-      sscbord = 2,                  // Start 2. Regler
-      eps = 0.005                   // Ende Regelung rel. Abw. von psoll
-      md = 1,                       // Dämpf./verst. der Regel.
-      ret.dp = Math.abs(psoll / pist - 1);
-    q = psoll / ts / mq;
-    f = md * ret.dp / bord;
-    if (typeof ret.dp == "number") {
-      // Gas
-      if (ret.dp > bord) {
-        if (q && q < 2) {
-          ret.sp1 = q;
-          ret.sp2 = 0;
+function fm3CalQsp(psoll, pist, Vopen) {
+    if (psoll && pist && typeof Vopen == "boolean") {
+        var ret = {},
+        pfill_ok = false,               // Fülldruck ok
+        mq = Vopen ? 0.0133 : 0.0155, // experimentall best. Steigungen
+        ts = 10,                      // Sollzeit
+        bord = 0.8,                   // Start Regelung rel. Abw. von psoll
+        sscbord = 2,                  // Start 2. Regler
+        eps = 0.005                   // Ende Regelung rel. Abw. von psoll
+        md = 1,                       // Dämpf./verst. der Regel.
+        ret.dp = Math.abs(psoll / pist - 1);
+        q = psoll / ts / mq;
+        f = md * ret.dp / bord;
+        if (typeof ret.dp == "number") {
+            // Gas
+            if (ret.dp > bord) {
+                if (q && q < 2) {
+                    ret.sp1 = q;
+                    ret.sp2 = 0;
+                }
+                else {
+                    ret.sp1 = q / 100;
+                    ret.sp2 = q;
+                }
+            };
+            // Kupplung
+            if (ret.dp <= bord) {
+                ret.pfill_ok = false;
+                if (q * f && q * f < 2) {
+                    ret.sp1 = q * f;
+                    ret.sp2 = q * f / 100;
+                }
+                else {
+                    ret.sp1 = q * f / 100;
+                    ret.sp2 = q * f;
+                }
+            };
+            // Bremse
+            if (ret.dp < eps || pist > psoll) {
+                ret.pfill_ok = true,
+                ret.sp1 = 0;
+                ret.sp2 = 0;
+            }
+            return ret;
         }
-        else {
-          ret.sp1 = q / 100;
-          ret.sp2 = q;
-        }
-      };
-      // Kupplung
-      if (ret.dp <= bord) {
-        ret.pfill_ok = false;
-        if (q * f && q * f < 2) {
-          ret.sp1 = q * f;
-          ret.sp2 = q * f / 100;
-        }
-        else {
-          ret.sp1 = q * f / 100;
-          ret.sp2 = q * f;
-        }
-      };
-      // Bremse
-      if (ret.dp < eps || pist > psoll) {
-        ret.pfill_ok = true,
-        ret.sp1 = 0;
-        ret.sp2 = 0;
-      }
-      return ret;
     }
-  }
 }
 exports.fm3CalQsp = fm3CalQsp;
 
@@ -79,28 +78,28 @@ exports.fm3CalQsp = fm3CalQsp;
  * eines gegebenen Vectors (bzw. Arrays).
  *
  *
- * @author wactbprot
+ *
  * @param x Array Datenreihe
  * @return res Object   res.mv (Mittelwert) res.sd (Standardabweichung) und re.N (Länge)
  */
 function vlStat(x) {
-  if (x && x.length > 2) {
-    var res = {},
-    mv = 0,
-      sdhelp = 0,
-      n = x.length;
-    mv = x.reduce(function(a, b) {
-      return a + b;
-    }) / n;
-    res.mv = mv;
-    res.N = n;
-    x.map(function(i) {
-      sdhelp += Math.pow((i - mv), 2);
-    });
-    sd = Math.sqrt(1 / (n - 1) * sdhelp);
-    res.sd = sd
-    return res;
-  }
+    if (x && x.length > 2) {
+        var res = {},
+        mv = 0,
+        sdhelp = 0,
+        n = x.length;
+        mv = x.reduce(function(a, b) {
+            return a + b;
+        }) / n;
+        res.mv = mv;
+        res.N = n;
+        x.map(function(i) {
+            sdhelp += Math.pow((i - mv), 2);
+        });
+        sd = Math.sqrt(1 / (n - 1) * sdhelp);
+        res.sd = sd
+        return res;
+    }
 };
 exports.vlStat = vlStat;
 
@@ -109,42 +108,42 @@ exports.vlStat = vlStat;
  * Testet Array auf numerische Einträge und sortiert ggf. aus
  *
  *
- * @author wactbprot
+ *
  * @param arr Array Datenreihe
  * @return res Object   res.Arr "sauberes" Array, res.Skip Indizes der verworfenen Einträge
  */
 function checkNumArr(arr) {
-  if (arr && Array.isArray(arr)) {
-    var res = {};
-    res.Arr = [],
-    res.Skip = [];
-    arr.map(function(v, i) {
-      if (isNumber(v)) {
-        res.Arr.push(v);
-      }
-      else {
-        res.Skip.push(i);
-      }
-    });
-    return res;
-  }
+    if (arr && Array.isArray(arr)) {
+        var res = {};
+        res.Arr = [],
+        res.Skip = [];
+        arr.map(function(v, i) {
+            if (isNumber(v)) {
+                res.Arr.push(v);
+            }
+            else {
+                res.Skip.push(i);
+            }
+        });
+        return res;
+    }
 };
 exports.checkNumArr = checkNumArr;
 
 /**
- * Entfernt Einträge aus einer Datenreihe. 
+ * Entfernt Einträge aus einer Datenreihe.
  *
  *
- * @author wactbprot
+ *
  * @param arr Array Datenreihe
  * @param arr Integer "skip" Index
  * @return res Array resultierendes Array
  */
 function rmByIndex(Arr, Idx) {
-  Idx.map(function(i) {
-    Arr.splice(i, 1);
-  });
-  return Arr
+    Idx.map(function(i) {
+        Arr.splice(i, 1);
+    });
+    return Arr
 }
 exports.rmByIndex = rmByIndex;
 
@@ -158,91 +157,91 @@ exports.rmByIndex = rmByIndex;
  * -- verworfen werden Pkt., die weiter
  *    als relative _d_border_ (0.6 entsp. 60%)
  *    von der Geraden weg liegen
- * 
  *
- * @author wactbprot
+ *
+ *
  * @param vec Array Datenreihe
  * @param tstart Array Startzeiten
  * @param tstop  Array Stopzeiten
  * @return res Object Ausgabe der slope Function
  */
 function vlSlope(vec, tstart, tstop) {
-  var t = [],
+    var t = [],
     rel_diff = [],
     iter_N = 5,
     again = true,
     d_border = 0.6;
-  for (var k = 0; k < tstart.length; k++) {
-    t.push((tstart[k] + tstop[k]) / 2)
-  }
-  var hs = slope(vec, t),
+    for (var k = 0; k < tstart.length; k++) {
+        t.push((tstart[k] + tstop[k]) / 2)
+    }
+    var hs = slope(vec, t),
     vlm = hs.bx,
     vlc = hs.Cx;
-  for (var m = 0; m < iter_N; m++) {
-    if (again) {
-      again = false;
-      for (var l = 0; l < t.length; l++) {
-        var predict_val = vlm * t[l] + vlc;
-        if (Math.abs((vec[l] - predict_val) / predict_val) > d_border) {
-          vec = rmByIndex(vec, [l]);
-          t = rmByIndex(t, [l]);
-          hs = slope(vec, t);
-          vlm = hs.bx;
-          vlc = hs.Cx;
-          again = true;
-        }
-      } // l
-    } //again
-  } //m
-  return slope(vec, t);
+    for (var m = 0; m < iter_N; m++) {
+        if (again) {
+            again = false;
+            for (var l = 0; l < t.length; l++) {
+                var predict_val = vlm * t[l] + vlc;
+                if (Math.abs((vec[l] - predict_val) / predict_val) > d_border) {
+                    vec = rmByIndex(vec, [l]);
+                    t = rmByIndex(t, [l]);
+                    hs = slope(vec, t);
+                    vlm = hs.bx;
+                    vlc = hs.Cx;
+                    again = true;
+                }
+            } // l
+        } //again
+    } //m
+    return slope(vec, t);
 };
 exports.vlSlope = vlSlope;
 
 /**
- * Reine Statistikfunktion zur Berechnung der Parameter der 
+ * Reine Statistikfunktion zur Berechnung der Parameter der
  * linearen Regression x~y.
  *
- * @author wactbprot
+ *
  * @param y Array y-Werte (z.B. Spannung, Druck)
  * @param x Array x-Werte (z.B. Zeit)
  * @return ret Object mit  Mittelwert X, Mittelwert Y, Korrelationskoeff R, Achsenabschnitt Cx, Steigungen bx, by
  */
 function slope(y, x) {
-  if (x.length == y.length) {
-    var ret = {},
-    sumX = 0,
-      XArr = [],
-      sumY = 0,
-      YArr = [],
-      remainN = 0,
-      SSxy = 0,
-      SSxx = 0,
-      SSyy = 0;
-    for (var i = 0; i < y.length; i++) {
-      if (isNumber(y[i]) && isNumber(x[i])) {
-        sumX = sumX + x[i];
-        XArr.push(x[i]);
-        sumY = sumY + y[i];
-        YArr.push(y[i]);
-        remainN++;
-      };
-    };
-    mvX = sumX / remainN;
-    mvY = sumY / remainN;
-    for (var j = 0; j < YArr.length; j++) {
-      SSxy = SSxy + (XArr[j] - mvX) * (YArr[j] - mvY);
-      SSxx = SSxx + Math.pow(XArr[j] - mvX, 2);
-      SSyy = SSyy + Math.pow(YArr[j] - mvY, 2);
-    };
-    ret.remainN = remainN
-    ret.mvX = mvX
-    ret.mvY = mvY
-    ret.bx = SSxy / SSxx;
-    ret.by = SSxy / SSyy;
-    ret.R = (SSxy * SSxy) / (SSxx * SSyy);
-    ret.Cx = mvY - ret.bx * mvX
-    return ret
-  }
+    if (x.length == y.length) {
+        var ret = {},
+        sumX = 0,
+        XArr = [],
+        sumY = 0,
+        YArr = [],
+        remainN = 0,
+        SSxy = 0,
+        SSxx = 0,
+        SSyy = 0;
+        for (var i = 0; i < y.length; i++) {
+            if (isNumber(y[i]) && isNumber(x[i])) {
+                sumX = sumX + x[i];
+                XArr.push(x[i]);
+                sumY = sumY + y[i];
+                YArr.push(y[i]);
+                remainN++;
+            };
+        };
+        mvX = sumX / remainN;
+        mvY = sumY / remainN;
+        for (var j = 0; j < YArr.length; j++) {
+            SSxy = SSxy + (XArr[j] - mvX) * (YArr[j] - mvY);
+            SSxx = SSxx + Math.pow(XArr[j] - mvX, 2);
+            SSyy = SSyy + Math.pow(YArr[j] - mvY, 2);
+        };
+        ret.remainN = remainN
+        ret.mvX = mvX
+        ret.mvY = mvY
+        ret.bx = SSxy / SSxx;
+        ret.by = SSxy / SSyy;
+        ret.R = (SSxy * SSxy) / (SSxx * SSyy);
+        ret.Cx = mvY - ret.bx * mvX
+        return ret
+    }
 }
 exports.slope = slope;
 
@@ -261,49 +260,49 @@ exports.slope = slope;
  * @return Number Zahl.
  */
 function extractValue(s) {
-  var regex = /^(\w*\s*)([-+]?[0-9]*\.?[0-9]+)([eE][-+]?[0-9]+)?(\s*\w*)$/;
-  return parseFloat(s.replace(regex, "$2$3"));
+    var regex = /^(\w*\s*)([-+]?[0-9]*\.?[0-9]+)([eE][-+]?[0-9]+)?(\s*\w*)$/;
+    return parseFloat(s.replace(regex, "$2$3"));
 }
 exports.extractValue = extractValue;
 
 /**
- * Extrahiert Float-Zahl aus String s.relay-add-test.js. 
+ * Extrahiert Float-Zahl aus String s.relay-add-test.js.
  *
- * @author wactbprot
+ *
  * @param  String str String mit enthaltener Zahl.
  * @return Number Zahl.
  */
 function extractF250(s) {
-  var regex = /^(A\s)([2-3]{2}\.?[0-9]{2,3})(C\r\n)$/
-  return parseFloat(s.replace(regex, "$2"));
+    var regex = /^(A\s)([2-3]{2}\.?[0-9]{2,3})(C\r\n)$/
+        return parseFloat(s.replace(regex, "$2"));
 }
 exports.extractF250 = extractF250
 
 /**
- * Extrahiert Float-Zahl aus String s.relay-add-test.js. 
+ * Extrahiert Float-Zahl aus String s.relay-add-test.js.
  *
- * @author wactbprot
+ *
  * @param  String str String mit enthaltener Zahl.
  * @return Number Zahl.
  */
 
 function extractAtmion(s) {
-  var regex = /^(0,\t)([0-9]{1}\.?[0-9]{4}[Ee][-+][0-9]{2})(\r)$/
-  return parseFloat(s.replace(regex, "$2"));
+    var regex = /^(0,\t)([0-9]{1}\.?[0-9]{4}[Ee][-+][0-9]{2})(\r)$/
+        return parseFloat(s.replace(regex, "$2"));
 }
 exports.extractAtmion = extractAtmion
 
 /**
- * Extrahiert Float-Zahl aus String s.relay-add-test.js. 
+ * Extrahiert Float-Zahl aus String s.relay-add-test.js.
  *
- * @author wactbprot
+ *
  * @param  String str String mit enthaltener Zahl.
  * @return Number Zahl.
  */
 
 function extractKeithley(s) {
-  var regex = /^([0-9]{1}\.?[0-9]{1,8}[Ee][-+][0-9]{2})/
-  return parseFloat(s.replace(regex, "$1"));
+    var regex = /^([0-9]{1}\.?[0-9]{1,8}[Ee][-+][0-9]{2})/
+        return parseFloat(s.replace(regex, "$1"));
 }
 exports.extractKeithley = extractKeithley
 
@@ -311,12 +310,12 @@ exports.extractKeithley = extractKeithley
  * Testet, ob sich der übergebene Parameter in eine Zahl wandeln lässt. Siehe auch:
  * http://stackoverflow.com/questions/18082/validate-numbers-in-javascript-isnumeric
  *
- * @author wactbprot
+ *
  * @param n String Zahlen-String
  * @return Boolean true, wenn Wandlung in Zahl erfolgreich war.
  */
 function isNumber(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
+    return !isNaN(parseFloat(n)) && isFinite(n);
 };
 exports.isNumber = isNumber
 
@@ -325,7 +324,7 @@ exports.isNumber = isNumber
  * Funktion erzeugt die üblich gewordene
  * Type-,  Value-, Unit- (Comment-) Struktur.
  *
- * @author wactbprot
+ *
  * @param t String  Type
  * @param v Number|Array  Value
  * @param u String  Unit
@@ -334,27 +333,27 @@ exports.isNumber = isNumber
  *
  */
 function vlRes(t, v, u, c) {
-  res = {
-    'Type': t,
-    'Value': v,
-    'Unit': u
-  }
-  if (c) {
-    res.Comment = c
-  }
-  return res
+    res = {
+        'Type': t,
+        'Value': v,
+        'Unit': u
+    }
+    if (c) {
+        res.Comment = c
+    }
+    return res
 };
 exports.vlRes = vlRes;
 
 /**
  *
  * Funktion gibt Status der Ventile an SE1 zurück.
- * Es muss für die toString Wandlung eine genügende 
+ * Es muss für die toString Wandlung eine genügende
  * Anzahl von Bits sichergestellt werden (2^32 = 4294967296:
- * 100000000000000000000000000000000) damit V[pos] 
- * sicher funktioniert (pos ändert sich bei führenden 0en). 
+ * 100000000000000000000000000000000) damit V[pos]
+ * sicher funktioniert (pos ändert sich bei führenden 0en).
  *
- * @author wactbprot
+ *
  * @param hexstr String Ventilrückmeldung
  * @param valve String Ventilbezeichnung
  * @return Object mit {'Valve_closed': true| false}
@@ -372,9 +371,9 @@ function se1ValveClosed(hexStr,valve) {
       },
     num = parseInt(hexStr,16) + BN,
     pat = num.toString(2).split('');
-    
+
     return {'Valve_closed': pat[V[valve]] == '1',
-	    'Valve_opened': pat[V[valve]] == '0' };
+            'Valve_opened': pat[V[valve]] == '0' };
 
 };
 exports.se1ValveClosed = se1ValveClosed;
