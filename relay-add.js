9704540e0d0a4415069ae3fb41f6593cf47eb92e
wactbprot <thsteinbock@web.de>
2013-01-24 11:10:17 +0100

    relay-add*.js +/- 	modified:   relay-add-test.js 	modified:   relay-add.js 	deleted:    relay-add.spec.js

--- relay-add.js
+++ relay-add.js
@@ -1,18 +1,39 @@
 /**
- * @author Thomas Bock (Thomas.Bock@ptb.de)
+ * Dedizierte Funktionen zur Vereinfachung des
+ * Postprocessing einer Task.
+ * 
+ * Eine hier definierte Funktion
+ * z.B. helloSandbox() kann über:
+ *
+ *  _.helloSandbox()
+ *
+ * im Postprocessing ausgeführt werden.
+ *
+ * @author wactbprot (thsteinbock@web.de)
  * version: 2013-01-18
  */
  
+/**
+ * Berechnet, in Abhängigkeit von Ziel- und Istdruck
+ * den aktuellen Sollfluß für die FM3 Fülldruckeinstellung.
+ *
+ *
+ * @author wactbprot
+ * @param psoll Number Solldruck in mbar
+ * @param psoll Number Istdruck in mbar
+ * @param Vopen Number kleines oder großes Volumen
+ * @return Object Zielfluß bzw Setpoint für Regler 1&2 
+ */
 function fm3CalQsp(psoll, pist, Vopen) { 
   if (psoll && pist && typeof Vopen == "boolean") {
     var ret = {},
-    pfill_ok = false, // Fülldruck ok
+    pfill_ok = false,               // Fülldruck ok
       mq = Vopen ? 0.0133 : 0.0155, // experimentall best. Steigungen
-      ts = 10, // sollzeit
-      bord = 0.8, // start Regelung rel. Abw. von psoll
-      sscbord = 2, // start 2. Regler
-      eps = 0.005 // ende Regelung rel. Abw. von psoll
-      md = 1, // Dämpf./verst. der Regel.
+      ts = 10,                      // Sollzeit
+      bord = 0.8,                   // Start Regelung rel. Abw. von psoll
+      sscbord = 2,                  // Start 2. Regler
+      eps = 0.005                   // Ende Regelung rel. Abw. von psoll
+      md = 1,                       // Dämpf./verst. der Regel.
       ret.dp = Math.abs(psoll / pist - 1);
     q = psoll / ts / mq;
     f = md * ret.dp / bord;
@@ -52,6 +73,16 @@ function fm3CalQsp(psoll, pist, Vopen) {
 }
 exports.fm3CalQsp = fm3CalQsp;
 
+
+/**
+ * Berechnet Mittelwert und Standardabweichung
+ * eines gegebenen Vectors (bzw. Arrays).
+ *
+ *
+ * @author wactbprot
+ * @param x Array Datenreihe
+ * @return res Object   res.mv (Mittelwert) res.sd (Standardabweichung) und re.N (Länge)
+ */
 function vlStat(x) {
   if (x && x.length > 2) {
     var res = {},
@@ -73,6 +104,15 @@ function vlStat(x) {
 };
 exports.vlStat = vlStat;
 
+
+/**
+ * Testet Array auf numerische Einträge und sortiert ggf. aus
+ *
+ *
+ * @author wactbprot
+ * @param arr Array Datenreihe
+ * @return res Object   res.Arr "sauberes" Array, res.Skip Indizes der verworfenen Einträge
+ */
 function checkNumArr(arr) {
   if (arr && Array.isArray(arr)) {
     var res = {};
@@ -91,6 +131,15 @@ function checkNumArr(arr) {
 };
 exports.checkNumArr = checkNumArr;
 
+/**
+ * Entfernt Einträge aus einer Datenreihe. 
+ *
+ *
+ * @author wactbprot
+ * @param arr Array Datenreihe
+ * @param arr Integer "skip" Index
+ * @return res Array resultierendes Array
+ */
 function rmByIndex(Arr, Idx) {
   Idx.map(function(i) {
     Arr.splice(i, 1);
@@ -100,15 +149,22 @@ function rmByIndex(Arr, Idx) {
 exports.rmByIndex = rmByIndex;
 
 /**
- * vaclab slope fn
+ * Berechnung der Steigung und zugeordneter Parameter.
  * - erzeugt zunächst aus den Vektoren
  *   tstart & tstop ein t
- * - versucht Ausreiser auf einfache art
- *   zu elim.
+ * - versucht Ausreiser auf einfache Art
+ *   zu eliminieren (Residuen > d_border werden verworfen)
  * -- max _iter_ Punkte werden verworfen
  * -- verworfen werden Pkt., die weiter
  *    als relative _d_border_ (0.6 entsp. 60%)
  *    von der Geraden weg liegen
+ * 
+ *
+ * @author wactbprot
+ * @param vec Array Datenreihe
+ * @param tstart Array Startzeiten
+ * @param tstop  Array Stopzeiten
+ * @return res Object Ausgabe der slope Function
  */
 function vlSlope(vec, tstart, tstop) {
   var t = [],
@@ -143,8 +199,13 @@ function vlSlope(vec, tstart, tstop) {
 exports.vlSlope = vlSlope;
 
 /**
- * reine slope fn
+ * Reine Statistikfunktion zur Berechnung der Parameter der 
+ * linearen Regression x~y.
  *
+ * @author wactbprot
+ * @param y Array y-Werte (z.B. Spannung, Druck)
+ * @param x Array x-Werte (z.B. Zeit)
+ * @return ret Object mit  Mittelwert X, Mittelwert Y, Korrelationskoeff R, Achsenabschnitt Cx, Steigungen bx, by
  */
 function slope(y, x) {
   if (x.length == y.length) {
@@ -194,8 +255,10 @@ exports.slope = slope;
  * Beispiele: "MEASURING     -1.13E-3 BLAFASEL" --> -0.00113
  *            "MEASURING .4711"                 -->  0.4711
  * </pre>
- * @param {string} str String mit enthaltener Zahl.
- * @return {number} Zahl.
+ *
+ * @author Rolf Niepraschk (Rolf.Niepraschk@ptb.de)
+ * @param  String str String mit enthaltener Zahl.
+ * @return Number Zahl.
  */
 function extractValue(s) {
   var regex = /^(\w*\s*)([-+]?[0-9]*\.?[0-9]+)([eE][-+]?[0-9]+)?(\s*\w*)$/;
@@ -203,17 +266,40 @@ function extractValue(s) {
 }
 exports.extractValue = extractValue;
 
+/**
+ * Extrahiert Float-Zahl aus String s.relay-add-test.js. 
+ *
+ * @author wactbprot
+ * @param  String str String mit enthaltener Zahl.
+ * @return Number Zahl.
+ */
 function extractF250(s) {
   var regex = /^(A\s)([2-3]{2}\.?[0-9]{2,3})(C\r\n)$/
   return parseFloat(s.replace(regex, "$2"));
 }
 exports.extractF250 = extractF250
 
+/**
+ * Extrahiert Float-Zahl aus String s.relay-add-test.js. 
+ *
+ * @author wactbprot
+ * @param  String str String mit enthaltener Zahl.
+ * @return Number Zahl.
+ */
+
 function extractAtmion(s) {
   var regex = /^(0,\t)([0-9]{1}\.?[0-9]{4}[Ee][-+][0-9]{2})(\r)$/
   return parseFloat(s.replace(regex, "$2"));
 }
 exports.extractAtmion = extractAtmion
+
+/**
+ * Extrahiert Float-Zahl aus String s.relay-add-test.js. 
+ *
+ * @author wactbprot
+ * @param  String str String mit enthaltener Zahl.
+ * @return Number Zahl.
+ */
 
 function extractKeithley(s) {
   var regex = /^([0-9]{1}\.?[0-9]{1,8}[Ee][-+][0-9]{2})/
@@ -224,8 +310,10 @@ exports.extractKeithley = extractKeithley
 /**
  * Testet, ob sich der übergebene Parameter in eine Zahl wandeln lässt. Siehe auch:
  * http://stackoverflow.com/questions/18082/validate-numbers-in-javascript-isnumeric
- * @param {string} n Zahlen-String
- * @return {boolean} true, wenn Wandlung in Zahl erfolgreich war.
+ *
+ * @author wactbprot
+ * @param n String Zahlen-String
+ * @return Boolean true, wenn Wandlung in Zahl erfolgreich war.
  */
 function isNumber(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
@@ -234,8 +322,15 @@ exports.isNumber = isNumber
 
 /**
  *
- * vaclab result erzeugt die üblich gewordene
- * Type-,  Value-, Unit- (Comment-) Struktur
+ * Funktion erzeugt die üblich gewordene
+ * Type-,  Value-, Unit- (Comment-) Struktur.
+ *
+ * @author wactbprot
+ * @param t String  Type
+ * @param v Number|Array  Value
+ * @param u String  Unit
+ * @param c String  Comment
+ * @return Object vaclab-Struktur
  *
  */
 function vlRes(t, v, u, c) {
@@ -250,3 +345,36 @@ function vlRes(t, v, u, c) {
   return res
 };
 exports.vlRes = vlRes;
+
+/**
+ *
+ * Funktion gibt Status der Ventile an SE1 zurück.
+ * Es muss für die toString Wandlung eine genügende 
+ * Anzahl von Bits sichergestellt werden (2^32 = 4294967296:
+ * 100000000000000000000000000000000) damit V[pos] 
+ * sicher funktioniert (pos ändert sich bei führenden 0en). 
+ *
+ * @author wactbprot
+ * @param hexstr String Ventilrückmeldung
+ * @param valve String Ventilbezeichnung
+ * @return Object mit {'Valve_closed': true| false}
+ *
+ */
+function se1ValveClosed(hexStr,valve) {
+
+    var BN = 4294967296,
+    V={V6:3,
+       V5:4,
+       V4:5,
+       V3:6,
+       V2:7,
+       V1:8,
+      },
+    num = parseInt(hexStr,16) + BN,
+    pat = num.toString(2).split('');
+    
+    return {'Valve_closed': pat[V[valve]] == '1',
+	    'Valve_opened': pat[V[valve]] == '0' };
+
+};
+exports.se1ValveClosed = se1ValveClosed;
