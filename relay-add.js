edca3762eade4386bb082dcff355f20064edbef2
Thomas Bock <bock04@e75467.berlin.ptb.de>
2014-01-30 11:16:37 +0100

    Temperatur an SE2 endet mit >>_C<<  Committer: Thomas Bock <bock04@e75467.berlin.ptb.de> 	modified:   relay-add.js

--- relay-add.js
+++ relay-add.js
@@ -366,6 +366,21 @@ function extractKeithleyVolt(s) {
 }
 exports.extractKeithleyVolt = extractKeithleyVolt;
 
+
+/**
+ * Extrahiert Float-Zahl aus String
+ * z.B. SE2 Temperaturen
+ *
+ * @author wactbprot
+ * @param  String str String mit enthaltener Zahl.
+ * @return Number Zahl.
+ */
+function extractKeithleyC(s) {
+  var regex = /^([+-][0-9]{1}\.?[0-9]{1,8}[Ee][-+][0-9]{2})(_C)/;
+  return strToNum(regex.exec(s), 1);
+}
+exports.extractKeithleyC = extractKeithleyC;
+
 /**
  * Extrahiert Float-Zahl aus String
  * z.B. SE1  Temperatursensoren
