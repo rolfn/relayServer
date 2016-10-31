1ce058bcadbd02610a4f35e796896431f9b134a6
Thomas Bock <bock04@e75467.berlin.ptb.de>
2016-10-31 14:31:18 +0100

    philips --> pm2535;rm prefix in regexpr

--- add/relay-add-PM2535.js
+++ add/relay-add-PM2535.js
@@ -1,6 +1,4 @@
-
 var x = require('./relay-add-MISC.js');
-
 /**
  * Extrahiert Float-Zahl aus String welcher vom Phillips
  * 2535 geliefert wird
@@ -9,10 +7,8 @@ var x = require('./relay-add-MISC.js');
  * @param  String str String mit enthaltener Zahl.
  * @return Number Zahl.
  */
-function extractPhillipsTemp(s) {
-  // TODO: Richtig: PHILIPS; besserer Name "extractPM2535Temp"
-  var regex = /^(TDC\s*)([-+]{1}[0-3]{3}\.?[0-9]{2}[E]{1}[-+]{1}[0-1]{2})(\n)$/;
-
-  return x.strToNum(regex.exec(s),2);
+function extractPM2535Temp(s) {
+    var regex = /([+][0-9]{3}\.?[0-9]{2}[E]{1}[-+]{1}[0-1]{2})(\n)$/;
+    return x.strToNum(regex.exec(s),1);
 }
-exports.extractPhillipsTemp= extractPhillipsTemp;
+exports.extractPM2535Temp = extractPM2535Temp;
