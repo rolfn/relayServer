3e791bf2577db2db50e1c013c9e07f631b5b03f7
niepra01 <rolf.niepraschk@ptb.de>
2016-12-01 10:57:31 +0100

    Fehler beseitigt, optimiert

--- add/relay-add-FESTO.js
+++ add/relay-add-FESTO.js
@@ -1,6 +1,11 @@
 
+/**
+ * @author Rolf Niepraschk (Rolf.Niepraschk@ptb.de)
+ * version: 2016-12-01
+ */
+
 var cfg = require('../config.js');
-// var inspect = require('util').inspect;
+//var inspect = require('util').inspect;
 var request = require('request');
 
 /**
@@ -9,8 +14,6 @@ var request = require('request');
  * und ggf. '"VNb":(1..32)' 
  * Alle anderen MODBUS-Parameter einschließlich "PostProcessing" setzt
  * diese Funktion.
- * 
- * @author Rolf Niepraschk
  */ 
 function getValveState(ctx) {// für PreProcessing
   ctx.Action = 'MODBUS';
@@ -23,9 +26,8 @@ function getValveState(ctx) {// für PreProcessing
 
 /**
  * Ist in der JSON-Struktur VNb angegeben, wird eine einzelner boolean-Wert
- * zurückgeliefert, sonst ein Array von 32 boolean-Werten.
+ * zurückgeliefert, sonst ein Array von 32 boolean-Werten (20 reale).
  *
- * @author Rolf Niepraschk
  * @return boolean-Wert oder Array von boolean-Werten
  */ 
 function getValveState2(ctx) {// für PostProcessing
@@ -45,28 +47,27 @@ function getValveState2(ctx) {// für PostProcessing
 }
 
 /**
- * Eingangs-JSON-Daten (Beispiel)
+ * Eingangs-JSON-Daten (Beispiel):
  *   {"Host":"172.30.56.46","PreProcessing":"_.setValveState(this);",
  *    "VNb": 9, Open":true}
  * Alle anderen MODBUS-Parameter einschließlich "PostProcessing" setzt
  * diese Funktion. Dazu wird vorher der bisherige Status des zugehörigen
  * Registers erfragt und der neu zu schreibende Wert errechnet.
- * 
- * @author Rolf Niepraschk
  */ 
 function setValveState(ctx) {// für PreProcessing
-  if (!ctx.Host || typeof ctx.VNb != 'number' || ctx.VNb < 1 || ctx.VNb > 20 ||
-    typeof ctx.Open == 'undfined') return;
+  var o = ctx._x;
+  if (!o.Host || typeof o.VNb != 'number' || o.VNb < 1 || o.VNb > 20 ||
+    typeof o.Open == 'undfined') return;
   var adr = 45407;
-  if (ctx.VNb > 16) adr+=2;
-  if (ctx.VNb > 12) adr+=2;
-  if (ctx.VNb >  8) adr+=2;
-  if (ctx.VNb >  4) adr+=2;
+  
+  if (o.VNb > 16) adr+=2;
+  if (o.VNb > 12) adr+=2;
+  if (o.VNb >  8) adr+=2;
+  if (o.VNb >  4) adr+=2;
   
   var jdata = {
-    Host:ctx.Host, VNb:ctx.VNb, Open:ctx.Open, 
-    Address: adr, Quantity:1, 
-    FunctionCode:'ReadHoldingRegisters', OutMode:'Uint16' 
+    Host:o.Host, VNb:o.VNb, Open:o.Open, Address:adr, Quantity:1,
+    Action:'MODBUS', FunctionCode:'ReadHoldingRegisters', OutMode:'Uint16' 
   };  
   var opts = {
     url: 'http://localhost:' + cfg.RELAY_PORT,
@@ -78,39 +79,44 @@ function setValveState(ctx) {// für PreProcessing
   };  
   function stateResponse(err, res, _d) {
     if (!err) {    
-      var d = typeof _d === 'string' ? JSON.parse(_d) : _d, v_data, v_old;
-      var v_old = d.Result, adr = 40003, v_data = ctx.VNb;
-      if (ctx.VNb > 16) {
+      var d = typeof _d === 'string' ? JSON.parse(_d) : _d, 
+        v_old = d.Result, adr = 40003, v_data = o.VNb;
+      if (o.VNb > 16) {
         adr++;
         v_data-=4;
       }
-      if (ctx.VNb > 12) {
+      if (o.VNb > 12) {
         adr++;
         v_data-=4;
       }
-      if (ctx.VNb >  8) {
+      if (o.VNb >  8) {
         adr++;
         v_data-=4;
       }
-      if (ctx.VNb >  4) {
+      if (o.VNb >  4) {
         adr++;
         v_data-=4;
       }
-      dtx.Address = adr;
-      v_data = 1 << 2*(v_data-1); 
-      dtx.Action = 'MODBUS';
-      dtx.FunctionCode = 'writeSingleRegister';
-      dtx.Value = dtx.Open ? v_old | (1<<v_data-1) : v_old & ~(1<<v_data-1);
+      if (o.VNb > 16) {
+        // TODO: Sonderbehandlung Ventile 17..20
+        v_data = 1 << 2*(v_data-1); // <-- anpassen!
+      } else {
+        v_data = 1 << 2*(v_data-1);
+      }
+      o.Address = adr;
+      o.Action = 'MODBUS';
+      o.FunctionCode = 'writeSingleRegister';
+      o.Value = o.Open ? v_old | v_data : v_old & ~v_data;
     }
-    ctx._removeCallback(stateResponse);   
+    ///console.log('stateResponse 1:' + inspect(opts));
+    ctx._busy--;   
   }
-  ctx._addCallback(stateResponse);     
-
+  ctx._busy++;     
   request(opts, stateResponse);// Abfrage des vorherigen Status
 }
 
 /**
- * Eingangs-JSON-Daten (Beispiel)
+ * Eingangs-JSON-Daten (Beispiel):
  *   {"Host":"172.30.56.46","PreProcessing":"_.getDigitalInput(this);"}
  * und ggf. '"PinNb":(1..32)' 
  * Alle anderen MODBUS-Parameter einschließlich "PostProcessing" setzt
@@ -121,16 +127,16 @@ function setValveState(ctx) {// für PreProcessing
 function getDigitalInput(ctx) {// für PreProcessing
   ctx.Action = 'MODBUS';
   ctx.FunctionCode = 'ReadHoldingRegisters';
-  ctx.Address = 45395; // ???
-  ctx.Quantity = 11; // ???
-  ctx.Skip = 1; // ???
+  ctx.Address = 45395; 
+  ctx.Quantity = 11; 
+  ctx.Skip = 1; 
   ctx.OutMode = '8Bits*';
   ctx.PostProcessing = 'Result=_.getDigitalInput2(this);';
 }
 
 /**
  * Ist in der JSON-Struktur PinNb angegeben, wird eine einzelner boolean-Wert
- * zurückgeliefert, sonst ein Array von 40 boolean-Werten.
+ * zurückgeliefert, sonst ein Array von 48 boolean-Werten.
  *
  * @author Rolf Niepraschk
  * @return boolean-Wert oder Array von boolean-Werten
@@ -138,11 +144,11 @@ function getDigitalInput(ctx) {// für PreProcessing
 function getDigitalInput2(ctx) {// für PostProcessing
   var x = ctx._x, PinNb = ctx._$.PinNb;
   var a = [];
-  for (var i=0; i<x.length; i++) {// 32?
-    a.push(x & (1<<i) ? true : false);
-  } // auf zwei 16Bit-Werte ausweiten.
-  if (typeof PinNb == 'number') {
-    return a[PinNb]
+  for (var i=0; i<x.length; i++) {
+    a.push(!!x[i]);
+  } 
+  if (typeof PinNb == 'number' && PinNb > 0 && PinNb <= x.length) {             
+    return a[PinNb-1]
   } else {
     return a
   }
