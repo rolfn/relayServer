7554d0b951af01ab3405b2a23ab2d811a611eb39
niepra01 <rolf.niepraschk@ptb.de>
2016-12-05 10:16:43 +0100

    Anpassung an neue Version von "nodemailer"

--- email.js
+++ email.js
@@ -1,6 +1,6 @@
 /**
  * @author Rolf Niepraschk (Rolf.Niepraschk@ptb.de)
- * version: 2015-07-08
+ * version: 2016-12-05
  */
 
 var cfg = require('./config.js');
@@ -9,17 +9,18 @@ var utils = require('./utils.js');
 var response = require('./response.js');
 var mailer = require('nodemailer');
 // npm install nodemailer
-/* Alternativen:
- 
-https://github.com/nodemailer/smtp-connection
-https://github.com/guileen/node-sendmail
-
-oder doch mit aktuellem 'nodemailer'?
-http://www.technicalkeeda.com/node/how-to-send-email-using-node-js
-
-*/
 
 var logger = cfg.logger;
+
+/**
+ * Lifert String von String-Array ("\\n" als Verbinder) oder String selbst. 
+ * @method getString
+ * @param x String oder String-Array
+ * @return {string} String
+ */
+function getString(x) {
+  if (Array.isArray(x)) return x.join('\n'); else return x;
+}
 
 /**
  * Konfiguration der für nodemailer nötigen Datenstrukturen und Vesenden einer E-Mail.
@@ -28,52 +29,48 @@ var logger = cfg.logger;
  * @param {object} js empfangene JSON-Struktur um weitere Daten ergänzt
  */
 function call(pRef, js) {
-  mailer.SMTP = {
-    host: (js.Host) ? js.Host : cfg.DEFAULT_SMTP_HOST,
-    port: tools.getInt(js.Port, cfg.DEFAULT_SMTP_PORT),
-    ssl:  (js.Ssl) ? js.Ssl : false,
-    use_authentication: (js.UseAuthentication) ? js.UseAuthentication : false,
-    user: (js.User) ? js.User : '',
-    pass: (js.Password) ? js.Password : ''
-  };
-  logger.debug('mailer', mailer.SMTP);
-  // Nur "From" und "To" sind zwingend.
-  var message = {sender:js.From,to:js.To,subject:js.Subject};
-  message.body = (js.Body) ? js.Body : '';
-  ///message.debug = tools.isDebug(99);
-  if (js.Cc) message.cc = js.Cc;
-  if (js.Bcc) message.bcc = js.Bcc;
-  if (js.ReplyTo) message.reply_to = js.ReplyTo;
-  if (js.Html) message.html = js.Html;
-  if ((js.Attachments) && Array.isArray(js.Attachments)) {
-    for (var i=0; i<js.Attachments.length; i++) {
-      var cont = js.Attachments[i].Contents;
-      if ((cont) && (js.Attachments[i].Filename)) {
-        if (!message.attachments) message.attachments = [];
-        message.attachments[i] = {};
-        message.attachments[i].filename = js.Attachments[i].Filename;
-        // Einfache Strings und String-Arrays unterstützen.
-        var buf = (Array.isArray(cont)) ? cont.join('') : cont;
-        message.attachments[i].contents = new Buffer(buf, "base64");
-      }
-    }
+  var options = {
+   host: js.Host || cfg.DEFAULT_SMTP_HOST,
+   port: js.Port || cfg.DEFAULT_SMTP_PORT,
+   logger: logger, // ???
+   debug:false 
   }
-  logger.debug('message', message);
-  var saveConsoleLog = console.log;
-  console.log = function (d) {
-    process.stderr.write(d + '\n');
-  };
+  // Nur "From" und "To" sind zwingend.
+  var mail = {
+    from: js.From || false,
+    to: js.To || false,
+    cc: js.Cs || null,
+    bcc: js.Bcc || null, 
+    replyTo: js.ReplyTo || null,
+    subject: js.Subject || '',
+    text: js.Text ? getString(js.Text) : '',
+    html: js.Text ? null : js.Html ? getString(js.Html) : null,
+    secure: typeof js.Secure == 'boolean' ? js.Secure : false
+  }
+  if (js.User) {
+    mail.auth = {};
+    mail.auth.user = js.User;
+    mail.auth.pass = js.Password || '';
+  }
+
+  if (!mail.from || !mail.to) response.prepareError(pRef, js, 'missing parameter');
+
+  var transporter = mailer.createTransport(options);
+  logger.debug('transporter', transporter);
+  logger.debug('message', mail.text || mail.html);
+
   utils.addStartTime(js);
-  // TODO: Array von smtp-Server gestatten, falls einer nicht funktioniert.
-  mailer.send_mail(message, function(error, success){
-    console.log = saveConsoleLog;
-    if (success) {
+  
+  transporter.sendMail(mail, function(err, res) {
+    if (err) {
+      response.prepareError(pRef, js, err.response);
+    } else {    
+      response.prepareResult(pRef, js, res.response);
       utils.addStopTime(js);
-      response.prepareResult(pRef, js, 'OK');
-    } else {
-      response.prepareError(pRef, js, error);
-    }
+    }  
+    transporter.close();
   });
+  
 }
 
 exports.call = call;
